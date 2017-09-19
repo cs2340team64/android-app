298e3fa6ab05e7a8b2ebfe372aaf330c38da4464
Diogo Monteiro <diogomont@gmail.com>
2017-09-19 01:29:12 -0400

    initial commit

---
+++ app/src/main/java/cs2340team64/dirtyrat/MainActivity.java
@@ -0,0 +1,13 @@
+package cs2340team64.dirtyrat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+    }
+}
